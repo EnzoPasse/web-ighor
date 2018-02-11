391ad4d3cefd55905f495993297abc5bfe4c9084
Enzo Passetti <enzo.passetti@gmail.com>
2018-02-11 19:20:48 -0300

    breadcrums

--- src/app/shared/breadcrums/breadcrums.component.ts
+++ src/app/shared/breadcrums/breadcrums.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
+
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-breadcrums',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class BreadcrumsComponent implements OnInit {
+  label = '';
 
-  constructor() { }
+  constructor(public router: Router, private title: Title, private meta: Meta) {
+    this.getDataRoute().subscribe(data => {
+      this.label = data.titulo;
 
-  ngOnInit() {
+      // agregando metaTags a la pagina
+      let metaTag: MetaDefinition = {
+        name: 'definition',
+        desciption: this.label
+      };
+      this.meta.updateTag(metaTag);
+
+      // agregando titulos a la pagina
+      this.title.setTitle(this.label);
+    });
   }
 
+  getDataRoute() {
+    return this.router.events
+      .filter(evento => evento instanceof ActivationEnd)
+      .filter((evento: ActivationEnd) => evento.snapshot.firstChild === null)
+      .map((evento: ActivationEnd) => evento.snapshot.data);
+  }
+
+  ngOnInit() {}
 }
