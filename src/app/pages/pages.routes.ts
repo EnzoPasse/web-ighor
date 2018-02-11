83adf3308c1aa0580dcb67c3d58e8529f3128b2d
Enzo Passetti <enzo.passetti@gmail.com>
2018-02-11 19:20:48 -0300

    breadcrums

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,7 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-
 import { PagesComponent } from './pages.component';
 import { PruebaComponent } from './prueba/prueba.component';
 import { PageNoFoundComponent } from '../shared/page-no-found/page-no-found.component';
@@ -14,21 +13,26 @@ const pagesRoutes: Routes = [
     component: PagesComponent,
     children: [
       { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'prueba', component: PruebaComponent },
-      { path: 'home', component: HomeComponent },
-      { path: 'account-setting', component: AccountSettingComponent },
+      {
+        path: 'prueba',
+        component: PruebaComponent,
+        data: { titulo: 'Pruebas' }
+      },
+      { path: 'home', component: HomeComponent, data: { titulo: 'Home' } },
+      {
+        path: 'account-setting',
+        component: AccountSettingComponent,
+        data: { titulo: 'Ajustes de Cuenta' }
+      },
       { path: '**', component: PageNoFoundComponent }
     ]
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild( pagesRoutes )],
+  imports: [RouterModule.forChild(pagesRoutes)],
   exports: [RouterModule]
 })
-
 export class PagesRoutes {}
 
-
 // export const PAGES_ROUTES = RouterModule.forChild( pagesRoutes );
-
